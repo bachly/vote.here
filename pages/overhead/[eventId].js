8672a64de1143228d916ad87fd1f42c94828294b
Bach Ly <bach@koaladigital.com.au>
2024-08-05 10:23:07 +1000

    Complete overhead

--- pages/overhead/[eventId].js
+++ pages/overhead/[eventId].js
@@ -1,14 +1,39 @@
 import { useEffect, useState } from "react";
 import { useInterval } from "../../lib/hooks";
 import { getOverhead, getPoll } from "../../lib/firebaseMethods";
+import _ from "underscore";
 
-const INTERVAL_REFRESHING_OVERHEAD = 2000000;
+const INTERVAL_REFRESHING_OVERHEAD = 3000;
 
-export default function ({ eventId, poll, currentPollId }) {
-    const [pollResults, setPollResults] = useState()
+export default function ({ currentPollId }) {
+    const [poll, setPoll] = useState()
+    const [votedAnswers, setVotedAnswers] = useState()
+    const [totalResponses, setTotalReponses] = useState()
+
+    useEffect(() => {
+        if (poll) {
+            const votedAnswers = {};
+            let total = 0;
+
+            _.sortBy(Object.keys(poll.answers)).map(answer => {
+                const totalVotes = Object.keys(poll.voterAnswers[answer] || {}).length;
+                total += totalVotes;
+                votedAnswers[answer] = totalVotes;
+            })
+
+            setVotedAnswers(votedAnswers);
+            setTotalReponses(total);
+        }
+    }, [poll])
+
+    useEffect(async () => {
+        const pollData = await getPoll({ pollId: currentPollId });
+        setPoll(pollData);
+    }, []);
 
     useInterval(async () => {
-        const pollResultData = await getPollResults({ currentPollId });
+        const pollData = await getPoll({ pollId: currentPollId });
+        setPoll(pollData);
     }, INTERVAL_REFRESHING_OVERHEAD);
 
     return <div className="min-h-screen bg-black">
@@ -23,31 +48,30 @@ export default function ({ eventId, poll, currentPollId }) {
             <header className="py-7 px-4 bg-neutral-900 text-white text-center text-5xl">
                 <div className="flex items-center justify-between">
                     <div className="text-left">{poll.question}</div>
-                    {/* <div className="text-right text-neutral-400">Total responses: {totalResponses}</div> */}
+                    <div className="text-right text-neutral-400">Total responses: {totalResponses}</div>
                 </div>
             </header>
 
             <div className="p-4 grid grid-cols-1 gap-2">
-                {Object.entries(poll.answers).map(([answer]) => {
-                    // const choice = poll.choices[key];
-                    // const percentage = answerCounter && answerCounter[choice] && `${(answerCounter[choice] / totalResponses * 100).toFixed(1)}%` || 0;
-                    // const counter = answerCounter && answerCounter[choice];
+                {_.sortBy(Object.keys(poll.answers)).map((answer) => {
+                    const counter = votedAnswers && votedAnswers[answer];
+                    const percentage = votedAnswers && votedAnswers[answer] && `${(votedAnswers[answer] / totalResponses * 100).toFixed(1)}%` || 0;
+
                     return <div key={answer} className="relative mt-1 block w-full py-4 px-2 text-green-300 border-2 border-green-600 rounded-lg text-3xl text-center">
                         <div className="relative z-10 flex items-center justify-center">
                             <span className="">{answer}</span>
-                            {/* {counter &&
+                            {counter > 0 &&
                                 <div className="absolute top-0 right-2 z-10 ml-2 text-white text-3xl flex items-center justify-center">
                                     {counter} responses • {percentage}
-                                </div>} */}
+                                </div>}
                         </div>
-                        {/* {counter &&
+                        {counter > 0 &&
                             <div className="absolute top-0 left-0 h-full bg-emerald-800 z-0"
                                 style={{ width: percentage }}>
-                            </div>} */}
+                            </div>}
                     </div>
                 })}
             </div>
-
         </>}
     </div>
 }
@@ -55,21 +79,16 @@ export default function ({ eventId, poll, currentPollId }) {
 
 export async function getServerSideProps(context) {
     const eventId = context.params.eventId;
-    let currentPollId, poll;
+    let currentPollId;
 
     if (eventId) {
         const overheadData = await getOverhead({ eventId });
         currentPollId = overheadData?.currentPollId;
-
-        if (currentPollId) {
-            poll = await getPoll({ pollId: currentPollId })
-        }
     }
 
     return {
         props: {
             eventId,
-            poll,
             currentPollId
         }
     }
